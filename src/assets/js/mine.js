f7c4d994c97a237baefaa3b8dae3a398799b0294
aria <aryakarami.ir@gmail.com>
2023-07-28 22:01:34 +0330

    first commit on branch master

---
+++ src/assets/js/mine.js
@@ -0,0 +1,64 @@
+/* ====== Top button ====== */
+$(window).on('scroll', function () {
+  if ($(this).scrollTop() >= 200) {
+    $('#top').css({
+      right: '20px'
+    });
+  } else {
+    $('#top').css({
+      right: '-80px'
+    });
+  }
+});
+
+
+jQuery(function () {
+  $('#top').on('click', function (e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0
+    }, 10);
+  });
+});
+
+/* ====== Works Tab ====== */
+jQuery(function () {
+  $('.all-btn').on('click', function (e) {
+    e.preventDefault();
+    $('div.site').show();
+    $('div.app').show();
+    $('div.poster').show();
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+  $('.sites-btn').on('click', function (e) {
+    e.preventDefault();
+    $('div.app').hide();
+    $('div.poster').hide();
+    $('div.site').show();
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+  $('.apps-btn').on('click', function (e) {
+    e.preventDefault();
+    $('div.site').hide();
+    $('div.poster').hide();
+    $('div.app').show();
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+  $('.posters-btn').on('click', function (e) {
+    e.preventDefault();
+    $('div.site').hide();
+    $('div.app').hide();
+    $('div.poster').show();
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+});
+
+/* ====== iTyped ====== */
+const titleElement = document.querySelector('#title')
+
+ityped.init(titleElement, { strings: ['با سرور های هتزنر آلمان', 'پشتیبانی 24 ساعته', 'در هفت روز هفته','با ما کیفیت','امنیت','و سرعت را تجربه کنید'] });
+
